f68e616cdea46e5d92701a82766eb364bbcea448
ankushbade <anksuh21bade@gmail.com>
2024-04-08 22:33:31 +0530

    initial commit

---
+++ src/main/java/com/example/bank/BankSecuritySec2Application.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSecuritySec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSecuritySec2Application.class, args);
+	}
+
+}
