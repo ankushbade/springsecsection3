6b6f666e5b5f50da3e81a96741023a2efbf9ab5d
ankushbade <anksuh21bade@gmail.com>
2024-04-09 14:32:15 +0530

    repository created to fetch data from table entity

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -13,3 +13,11 @@ CREATE TABLE `authorities` (
 
 INSERT IGNORE INTO `users` VALUES (NULL, 'spring', '12345', '1');
 INSERT IGNORE INTO `authorities` VALUES (NULL, 'spring', 'write');
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
